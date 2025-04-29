35cb8c1aec243a71c98e5b38bff37f61cd237383
opulencedeveloper <victorkudos@gmail.com>
2025-04-29 15:21:05 +0100

    initial commit

--- src/components/application/dashboard/home/tracking.tsx
+++ src/components/application/dashboard/home/tracking.tsx
@@ -2,7 +2,7 @@ import TrackingCard from "./tracking-card";
 
 export default function Tracking() {
   return (
-    <div className="flex flex-col max-w-[900px] md:max-w-full gap-y-2 mt-2 pb-10">
+    <div className="flex flex-col max-w-[900px] md:max-w-full gap-y-2 mt-2 pb-20">
       <TrackingCard />
       <TrackingCard />
       <TrackingCard />
